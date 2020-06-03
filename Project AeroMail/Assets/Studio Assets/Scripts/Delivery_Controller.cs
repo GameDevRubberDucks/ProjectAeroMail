f362ca5a29b73b601d4cadeb423c825447677c4f
Dan <dmaccormick@hotmail.com>
2020-06-03 15:30:29 -0400

    Implemented backend for delivery system
    - Created the Delivery_Zone class for the start and end zones of the delivery
    > Also created a prefab for this with a trigger zone and camera
    - Created the Delivery structure that manages each delivery
    - Created the Delivery_Controller which handles the active delivery

---
+++ Project AeroMail/Assets/Studio Assets/Scripts/Delivery_Controller.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+public class Delivery_Controller : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public List<Delivery> m_allDeliveries;
+
+
+
+    //--- Private Variables ---//
+    private Delivery_ZoneChangeEvent m_onTargetZoneChanged;
+    private Delivery m_activeDelivery;
+    private Delivery_Zone m_targetZone;
+    private int m_activeID;
+
+
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        // Activate the first delivery
+        ActivateDelivery(0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Delivery trigger zones need to be handled as the possible start or end of a mission
+        if (other.tag == "Delivery_Zone")
+            HandleZoneEntry(other.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        // Clear all of the listeners that are left on the events
+        m_onTargetZoneChanged.RemoveAllListeners();
+    }
+
+
+
+    //--- Methods ---//
+    public void NextDelivery()
+    {
+        // Activate the next delivery in the sequence
+        // TODO: Do something special if that is the end of the deliveries (ex: show game over?)
+        ActivateDelivery(m_activeID + 1);
+    }
+
+    public void ActivateDelivery(int _id)
+    {
+        // Ensure the new ID is in the correct range
+        Assert.IsTrue(_id >= 0 && _id < m_allDeliveries.Count, "_id must be a valid index for m_allDeliveries");
+
+        // Set the new delivery ID
+        m_activeID = _id;
+
+        // Activate the delivery itself
+        m_activeDelivery = m_allDeliveries[_id];
+
+        // The new target zone should be the starting point for this delivery
+        TargetZone = m_activeDelivery.m_startZone;
+    }
+
+    public void HandleZoneEntry(GameObject _zone)
+    {
+        // We first need to grab the zone component from the trigger's parent
+        Delivery_Zone zoneComp = _zone.GetComponentInParent<Delivery_Zone>();
+
+        // If the zone is part of the active mission, we need to handle it as such
+        // Otherwise, we can just ignore it since this is an irrelevant zone
+        if (m_activeDelivery.m_startZone == zoneComp)
+        {
+            // We just entered the starting zone for this delivery
+            // This means that our new target is the ending zone for it
+            // TODO: Add feedback here
+            TargetZone = m_activeDelivery.m_endZone;
+        }
+        else if (m_activeDelivery.m_endZone == zoneComp)
+        {
+            // We just entered the ending zone for this delivery
+            // This means that we should start the next delivery
+            // TODO: Add feedback here
+            NextDelivery();
+        }
+    }
+
+
+
+    //--- Setters and Getters ---//
+    public Delivery_ZoneChangeEvent OnTargetZoneChanged
+    {
+        get => m_onTargetZoneChanged;
+    }
+
+    public Delivery_Zone TargetZone
+    {
+        get => m_targetZone;
+        set
+        {
+            // Store the new target zone value
+            m_targetZone = value;
+
+            // Trigger the event to indicate that the target zone has been updated
+            m_onTargetZoneChanged.Invoke(m_targetZone);
+        }
+    }
+}
---
+++ Project AeroMail/Assets/Studio Assets/Scripts/Delivery_Zone.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Delivery_Zone : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public Camera m_zoneCamera;
+}
