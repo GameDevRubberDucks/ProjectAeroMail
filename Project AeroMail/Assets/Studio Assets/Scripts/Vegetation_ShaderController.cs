749bb48167f565944bb48cd7bb8d08950097e2eb
Dan <dmaccormick@hotmail.com>
2020-06-11 19:16:59 -0400

    Updated the vegetation shader to consider the player position
    The vegetation now bends out of the way of the player as your approach.  It's a bit buggy but it is getting there. Final stage is to consider player speed as well

---
+++ Project AeroMail/Assets/Studio Assets/Scripts/Vegetation_ShaderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetation_ShaderController : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public Transform m_playerObj;
+    
+
+
+    //--- Private Variables ---//
+    private Material m_mat;
+
+
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        // Init the private variables
+        m_mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        // Grab the player's updated position and pass it to the shader
+        Vector3 newPos = m_playerObj.position;
+        m_mat.SetVector("_playerPosition", new Vector4(newPos.x, newPos.y, newPos.z, 0.0f));
+    }
+}
