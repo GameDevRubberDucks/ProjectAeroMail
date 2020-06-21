7978fa759c49fa94c9f539da3b1412f2db492c98
Kody Wood <kodymrwood14@gmail.com>
2020-06-20 20:36:27 -0400

    [ADD] Island

---
+++ Project AeroMail/Assets/Studio Assets/Scripts/Camera_Collider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Camera_Collider : MonoBehaviour
+{
+    public GameObject postProcessing;
+    public Volume postPro;
+    //public TextureParameter[] lookUp;
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        //postPro.GetComponent<ColorLookup>().texture = lookUp[0];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Water")
+        {
+            //postProcessing.SetActive(true);
+            postProcessing.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            //postProcessing.SetActive(false);
+            postProcessing.gameObject.SetActive(false);
+        }
+    }
+}
--- Project AeroMail/Assets/Studio Assets/Scripts/Player_Particles.cs
+++ Project AeroMail/Assets/Studio Assets/Scripts/Player_Particles.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class Player_Particles : MonoBehaviour
 {
     //---Setup Variables---//
-    private TrailRenderer[] trailSystems;
-    private PlayerControllerScript pController;
-
-
-    //---Particle Variables---//
-    [Tooltip("Controls how long the trail is on speed (Higher the shorter)")]
-    public float timeFraction = 1000.0f;
+    public GameObject trailSystems;
+    public GameObject bubbleSystems;
 
     //Start is called before the first frame update
     void Start()
     {
-        trailSystems = GetComponentsInChildren<TrailRenderer>();
-        pController = GetComponent<PlayerControllerScript>();
+        trailSystems.SetActive(true);
+        bubbleSystems.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //trailSystems[0].time = pController.currentSpeed / timeFraction;
-        //trailSystems[1].time = pController.currentSpeed / timeFraction;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+
+            
+            trailSystems.SetActive(false);
+            bubbleSystems.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+
+            trailSystems.SetActive(true);
+            bubbleSystems.SetActive(false);
+        }
     }
 }
